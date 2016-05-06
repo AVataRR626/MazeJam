2bd38750448415c308906e224b0b2dac066c1081
AVataRR626 <matt.cabanag@gmail.com>
2016-05-07 01:53:01 +1000

    Timo's flicker
    Player lights now flicker thanks to Timo
    Player lights now relative to overall maze light.

---
+++ UnityProject/Assets/02-Scripts/Flicker.cs
@@ -0,0 +1,40 @@
+﻿//Flicker class! By Timo;
+//slightly modified by Matt
+using UnityEngine;
+using System.Collections;
+public class Flicker : MonoBehaviour
+{   
+
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float amount;
+
+    [SerializeField]
+    float startIntensity;
+
+    [SerializeField]
+    float startRange;
+
+    [SerializeField]
+    float rangeFactor;
+
+    float flicker;
+
+    Light light;
+    Light globLight;
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+        globLight = FindObjectOfType<MazeLightManager>().GetComponent<Light>();
+    }
+    void Update()
+    {
+        flicker = Mathf.Sin(Time.time * speed);
+        light.intensity = (startIntensity-globLight.intensity) + Time.deltaTime * amount * flicker;
+        light.range = startRange + flicker * rangeFactor;
+    }
+}
