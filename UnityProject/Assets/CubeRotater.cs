cbdfdff885324388cde5ab5f2dcc66b9fe355c46
ADiggins <ajpdiggins@gmail.com>
2016-05-07 23:28:54 +1000

    Consolidated all scenes

---
+++ UnityProject/Assets/CubeRotater.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CubeRotater : MonoBehaviour {
+
+	void Awake () {
+        for (int k = transform.childCount - 1; k >= 0; k--)
+        {
+            float rotation = 90;
+            rotation *= (int)Random.Range(0, 4);
+            Debug.Log(rotation);
+            transform.GetChild(k).Rotate(new Vector3(0, rotation, 0));
+        }
+    }
+}
---
+++ UnityProject/Assets/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public void QuitGame () {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+    public void StartLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+        /*
+        ïf(name == "Convergence") {
+            
+        }
+        "Divergence"
+        "Destiny"
+        "PointToPoint"
+            "Conquest"
+            */
+
+    }
+
+
+
+
+}
