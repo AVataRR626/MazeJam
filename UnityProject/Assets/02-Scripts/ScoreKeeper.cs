80f40bd275d613cbef48fd5b758890883ddf8538
AVataRR626 <matt.cabanag@gmail.com>
2016-05-08 14:24:20 +1000

    scoring system
    blue and red wins get tracked..
    displayed at the "prepare to memorise maze" screen....
    reset scores in the slide show level

---
+++ UnityProject/Assets/02-Scripts/ScoreKeeper.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public bool resetScores;
+    public bool incScore;
+    public int winner;
+    public Text myText;
+
+    int p1Score;
+    int p2Score;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myText = GetComponent<Text>();
+
+        if(resetScores)
+        {
+            PlayerPrefs.SetInt("P1Score", 0);
+            PlayerPrefs.SetInt("P2Score", 0);
+        }
+
+        p1Score = PlayerPrefs.GetInt("P1Score");
+        p2Score = PlayerPrefs.GetInt("P2Score");
+
+        if (incScore)
+        {
+            if (winner == 0)
+                p1Score++;
+
+            if (winner == 1)
+                p2Score++;
+
+            PlayerPrefs.SetInt("P1Score", p1Score);
+            PlayerPrefs.SetInt("P2Score", p2Score);
+        }
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(myText != null)
+        {
+            myText.text = "Blue: " + p1Score.ToString() + " | Red: " + p2Score.ToString();
+        }
+	}
+
+}
