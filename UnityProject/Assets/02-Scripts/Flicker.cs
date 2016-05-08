9f1f81872f58bb052c1b80ba6e9a69bda5a6fd17
AVataRR626 <matt.cabanag@gmail.com>
2016-05-08 12:42:52 +1000

    light range and player tweaks
    woo

--- UnityProject/Assets/02-Scripts/Flicker.cs
+++ UnityProject/Assets/02-Scripts/Flicker.cs
@@ -31,23 +31,21 @@ public class Flicker : MonoBehaviour
     Light globLight;
 
     float offset;
+    float origRange;
 
     void Awake()
     {
         light = GetComponent<Light>();
         globLight = FindObjectOfType<MazeLightManager>().GetComponent<Light>();
         offset = Random.Range(0,10);
+        origRange = light.range;
     }
     void Update()
     {
         flicker = Mathf.Sin(offset + Time.time * speed);
 
         light.intensity = startIntensity + Time.deltaTime * amount * flicker;
-
-        if(Input.GetKey(reduceKey))
-        {
-            light.intensity *= 0.5f;
-        }
+        
 
         if (isDimmedByGlobalLight)
         {
@@ -58,5 +56,11 @@ public class Flicker : MonoBehaviour
         }
 
         light.range = startRange + flicker * rangeFactor;
+
+        if (Input.GetKey(reduceKey))
+        {
+            light.intensity *= 0.75f;
+            light.range *= 0.9f;
+        }
     }
 }
